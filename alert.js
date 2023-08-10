da8b90f7dc7d8983826f348e6d63c30656883e74
Stephen Johnson-Tseu <stephjohnsons@me.com>
2023-08-10 12:25:35 +0800

    amended

--- alert.js
+++ alert.js
@@ -1,16 +1,5 @@
-<Alert show={show} variant="success">
-        <Alert.Heading>Alert</Alert.Heading>
-        <p>
-          Please note that this project was inspired by 
-          <a href="https://instagram.com/ui_shreyash">
-            @ui_shreyash
-          </a>
-          , and is not intended for commercial purposes or sale. 
-        </p>
-        <hr />
-        <div className="d-flex justify-content-end">
-          <Button onClick={() => setShow(false)} variant="outline-success">
-            Close me
-          </Button>
-        </div>
-      </Alert>
+function disclaimerAlert() {
+   alert("Please note that this project was inspired by @ui_shreyash, and is not intended for commercial purposes or sale.");
+}
+
+onload="disclaimerAlert()"
